2b63ab7124411f4477af9121c79c74083de0ea8d
RamAvni <ramavni@hilma.tech>
2026-03-02 20:44:05 +0200

    added some comments for qol

--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
+echo "Removing existing build directory"
 rm -rf build
 
+echo "Compiling..."
 tsc # Build the whole project
+echo "Copying static files..."
 rsync -a --exclude='*.ts' src/web/static build/web # Copy the static files into build
 
+echo "Running the Server:"
 node ./build/web/main.js # Run web server
